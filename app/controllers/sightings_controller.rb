c548764a1ac25c364f0a9c922a26a5cbabc16aa6
Riley Slayden <slaydenriley@gmail.com>
2020-07-01 18:22:56 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,10 @@
 class SightingsController < ApplicationController
   def show
     sighting = Sighting.find_by(id: params[:id])
-    render json: { id: sighting.id, bird: sighting.bird, location: sighting.location }
+    if sighting
+      render json: sighting.to_json(include: [:bird, :location])
+    else
+      render json: { message: 'No sighting found with that id' }
+    end
   end
 end
